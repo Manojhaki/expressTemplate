7892d37e73642087576b40a805b0b141ac08fb2a
Manoj Pun <manojpun@manojs-mbp.myfiosgateway.com>
2022-02-02 12:27:04 -0500

    adding morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan')
 //load env vars
 
 dotenv.config({ path: './config/config.env' });
@@ -13,13 +13,17 @@ const works = require('./routes/work');
 
 
 
+//dev logging middle ware
+if (process.env.NODE_ENV === 'development') {
+
+    app.use(morgan('dev'));
+}
+
 //mount routers
 app.use(express.json());
 
 app.use('/api/v1/works', works);
 const server = app.listen(PORT, console.log("Server is running in port: ", PORT));
-
-
 
 // Handle undhandled promise rejections - kill app
 process.on("unhandledRejection", (err, promise) => {
