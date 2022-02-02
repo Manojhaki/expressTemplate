e26106e8fc3a40c5cd4f24a321120a00166851e6
Manoj Pun <manojpun@manojs-mbp.myfiosgateway.com>
2022-02-02 11:47:04 -0500

    adding CRUD

--- server.js
+++ server.js
@@ -10,6 +10,37 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 const server = app.listen(PORT, console.log("Server is running in port: ", PORT));
+
+
+app.get('/api/v1/work', (req, res) => {
+
+    res.status(200).json({ success: true, msg: "show all work" });
+
+});
+
+app.get('/api/v1/work/:id', (req, res) => {
+
+    res.status(200).json({ success: true, msg: `get ${req.params.id} work` });
+
+});
+
+app.post('/api/v1/work', (req, res) => {
+
+    res.status(200).json({ success: true, msg: "create new work" });
+
+});
+
+app.put('/api/v1/work/:id', (req, res) => {
+
+    res.status(200).json({ success: true, msg: `update ${req.params.id} work` });
+
+});
+
+app.delete('/api/v1/work/:id', (req, res) => {
+
+    res.status(200).json({ success: true, msg: `delete ${req.params.id} work` });
+
+});
 // Handle undhandled promise rejections - kill app
 process.on("unhandledRejection", (err, promise) => {
     console.log(`Error: ${err.message}`);
