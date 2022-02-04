777cba5ff671cc73795b308e1ff3debdae177468
Manojhaki <manoj.haki@gmail.com>
2022-02-04 01:00:45 -0500

    adding the create, post, delete functionality

--- controllers/work.js
+++ controllers/work.js
@@ -4,17 +4,38 @@ const Work = require('../models/Work');
 // @desc get all the works
 // @route GET /api/v1/works
 // @access Public
-exports.getWorks = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "show all work" });
-}
+exports.getWorks = async (req, res, next) => {
+
+    try {
+        const works = await Work.find();
+        res.status(200).json({ success: true, count: works.length, data: works });
+
+    } catch (error) {
+        res.status(400).json({ success: false });
+
+    }
+
+};
 
 // @desc get single the work
 // @route GET /api/v1/works/:id
 // @access Public
-exports.getWork = (req, res, next) => {
+exports.getWork = async (req, res, next) => {
+
+    try {
+        const work = await Work.findById(req.params.id);
+
+        if (!work) {
+            return res.status(400).json({ success: false })
+        }
+
+        res.status(200).json({ success: true, data: work })
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
     res.status(200).json({ success: true, msg: `get ${req.params.id} work` });
 
-}
+};
 
 // @desc create new work
 // @route POST /api/v1/works
@@ -22,26 +43,43 @@ exports.getWork = (req, res, next) => {
 exports.createWork = async (req, res, next) => {
     /// the create error risising
 
+
     const work = await Work.create(req.body);
     res.status(201).json({
         success: true,
         data: work
     });
 
-}
+};
 
 // @desc update work
 // @route PUT /api/v1/works/:id
 // @access Private
-exports.updateWork = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `update ${req.params.id} work` });
+exports.updateWork = async (req, res, next) => {
+    const work = await Work.findByIdAndUpdate(req.params.id, req.body, {
 
-}
+        new: true,
+        runValidators: true
+    });
+
+    if (!work) {
+        return res.status(400).json({ success: false })
+    }
+    res.status(200).json({ success: true, data: work });
+};
 
 // @desc delete work
 // @route DELETE /api/v1/works/:id
 // @access Private
-exports.deleteWork = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `delete ${req.params.id} work` });
+exports.deleteWork = async (req, res, next) => {
+    try {
+        const work = await Work.findByIdAndDelete(req.params.id);
+        if (!work) {
+            res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+    } catch (error) {
+        res.status(400).json({ success: false });
 
-}
+    }
+};
